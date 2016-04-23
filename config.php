8247505bc4814ad84c52613f9e0651e4ab5680a9
ywyang <y.w.yang@163.com>
2016-04-23 21:30:21 +0800

    this is a test

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 	header('Content-Type:text/html; charset=utf-8');
-
+//test by asfasdfas
 	define('DB_HOST', SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT);
 	define('DB_USER', SAE_MYSQL_USER);
 	define('DB_PWD', SAE_MYSQL_PASS);
